4e19ad61af4da3df0e71d143395899c092549462
Oskar Tołkacz <oskar@tolkacz.pl>
2024-06-06 21:51:06 -0400

    Add game loop and message broadcasting

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bytes"
 	"flag"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"sync"
 	"text/template"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
@@ -50,68 +52,22 @@ func getIndexHandler() http.HandlerFunc {
 type Token string
 
 type Client struct {
-	Token Token
-	Conn  *websocket.Conn
+	Token           Token
+	Conn            *websocket.Conn
+	SendPreparedMsg chan *websocket.PreparedMessage
+	SendBytes       chan []byte
 }
 
 func NewClient(conn *websocket.Conn) *Client {
 	return &Client{
-		Token: Token(uuid.New().String()),
-		Conn:  conn,
+		Token:           Token(uuid.New().String()),
+		Conn:            conn,
+		SendPreparedMsg: make(chan *websocket.PreparedMessage),
+		SendBytes:       make(chan []byte),
 	}
 }
 
-type Clients struct {
-	sync.RWMutex
-	values map[Token]*Client
-}
-
-func NewClients() *Clients {
-	return &Clients{
-		values: make(map[Token]*Client),
-	}
-}
-
-func (clients *Clients) Put(client *Client) {
-	clients.Lock()
-	defer clients.Unlock()
-	clients.values[client.Token] = client
-}
-
-func (clients *Clients) Get(token Token) *Client {
-	clients.Lock()
-	defer clients.Unlock()
-	return clients.values[token]
-}
-
-func (clients *Clients) Remove(client *Client) {
-	clients.Lock()
-	defer clients.Unlock()
-	delete(clients.values, client.Token)
-}
-
-type Game struct {
-	Clients   *Clients
-	AddClient chan *Client
-}
-
-func NewGame() *Game {
-	return &Game{
-		Clients:   NewClients(),
-		AddClient: make(chan *Client),
-	}
-}
-
-func (g *Game) ManageClients() {
-	for {
-		select {
-		case client := <-g.AddClient:
-			g.Clients.Put(client)
-		}
-	}
-}
-
-func HandleClient(g *Game, c *Client) {
+func (c *Client) HandleClient(g *Game) {
 	done := make(chan struct{})
 
 	go func() {
@@ -134,12 +90,148 @@ func HandleClient(g *Game, c *Client) {
 
 	for {
 		select {
+		case msg := <-c.SendPreparedMsg:
+			slog.Debug("Sending prepared message.")
+			err := c.Conn.WritePreparedMessage(msg)
+			if err != nil {
+				slog.Warn("Failed to send prepared message.")
+				return
+			}
+		case msg := <-c.SendBytes:
+			err := c.Conn.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				slog.Warn("Failed to send message.")
+				return
+			}
 		case <-done:
 			slog.Debug("Client disconnected.", "token", c.Token)
 			c.Conn.Close()
 			return
-
+		case <-time.After(time.Second):
+			slog.Debug("Client handler running.")
 		}
+	}
+}
+
+type Clients struct {
+	sync.RWMutex
+	Values map[Token]*Client
+}
+
+func NewClients() *Clients {
+	return &Clients{
+		Values: make(map[Token]*Client),
+	}
+}
+
+func (clients *Clients) Put(client *Client) {
+	clients.Lock()
+	defer clients.Unlock()
+	clients.Values[client.Token] = client
+}
+
+func (clients *Clients) Get(token Token) *Client {
+	clients.Lock()
+	defer clients.Unlock()
+	return clients.Values[token]
+}
+
+func (clients *Clients) Remove(client *Client) {
+	clients.Lock()
+	defer clients.Unlock()
+	delete(clients.Values, client.Token)
+}
+
+type GameStateNotStarted struct {
+}
+
+type GameStateTransition struct {
+	TransitionEnd time.Time
+	Message       string
+	TargetState   any
+}
+
+type Game struct {
+	Clients   *Clients
+	AddClient chan *Client
+	Broadcast chan []byte
+
+	GameState any
+}
+
+func NewGame() *Game {
+	return &Game{
+		Clients:   NewClients(),
+		AddClient: make(chan *Client),
+		Broadcast: make(chan []byte),
+		GameState: GameStateNotStarted{},
+	}
+}
+
+func (g *Game) ChangeState(newState any) {
+	g.GameState = newState
+}
+
+func (g *Game) ManageClients() {
+	for {
+		select {
+		case client := <-g.AddClient:
+			g.Clients.Put(client)
+		case msg := <-g.Broadcast:
+			preparedMsg, err := websocket.NewPreparedMessage(websocket.TextMessage, msg)
+			if err != nil {
+				slog.Error("Failed to prepare message.")
+			} else {
+				slog.Debug("PreparedMessage ready!")
+				for _, client := range g.Clients.Values {
+					if client != nil {
+						slog.Debug("Sending msg to client", "token", client.Token)
+						client.SendPreparedMsg <- preparedMsg
+					}
+				}
+			}
+		}
+	}
+}
+
+func (g *Game) Run() {
+	const REFRESH_RATE = 50 * time.Millisecond
+	for {
+		select {
+		case <-time.After(REFRESH_RATE):
+			g.run()
+		}
+	}
+}
+
+func (g *Game) run() {
+	const GAME_START_TRANSITION_TIME = 5 * time.Second
+
+	switch state := g.GameState.(type) {
+	case GameStateNotStarted:
+		if len(g.Clients.Values) > 0 {
+			transition := GameStateTransition{
+				TransitionEnd: time.Now().UTC().Add(GAME_START_TRANSITION_TIME),
+				Message:       "Waiting to start a new game.",
+				TargetState:   GameStateNotStarted{},
+			}
+			g.GameState = transition
+			dateStr, err := transition.TransitionEnd.MarshalText()
+			if err != nil {
+				slog.Error("Failed to marshal transition end.")
+				return
+			}
+			msg := fmt.Sprintf("Transition\n%v\n%v", string(dateStr), transition.Message)
+			g.Broadcast <- []byte(msg)
+		}
+	case GameStateTransition:
+		now := time.Now().UTC()
+		if now.After(state.TransitionEnd) {
+			g.GameState = state.TargetState
+		}
+
+	default:
+		slog.Error("Unexpected game state.", "GameState", g.GameState)
 	}
 }
 
@@ -170,7 +262,7 @@ func makeWsConnection(game *Game, w http.ResponseWriter, r *http.Request) {
 	}
 	slog.Debug("Client connected.", "token", client.Token)
 	game.AddClient <- client
-	go HandleClient(game, client)
+	go client.HandleClient(game)
 }
 
 func main() {
@@ -183,6 +275,7 @@ func main() {
 
 	game := NewGame()
 	go game.ManageClients()
+	go game.Run()
 
 	http.HandleFunc("GET /{$}", getIndexHandler())
 	http.Handle(
